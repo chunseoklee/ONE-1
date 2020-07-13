a7e5b33e75fda188347ab181af7ed1e4b6d6f641
Chunseok Lee <chunseok.lee@samsung.com>
2020-07-13 15:14:34 +0900

    sigmoid opt patch
    ONE-DCO-1.0-Signed-off-by: Chunseok Lee <chunseok.lee@samsung.com>

--- compute/cker/include/cker/operation/Logistic.h
+++ compute/cker/include/cker/operation/Logistic.h
@@ -20,6 +20,7 @@
 
 #include "cker/Shape.h"
 #include "cker/eigen/Utils.h"
+#include "cker/NEMath.inl"
 
 #include <cmath>
 #include <Eigen/Core>
@@ -37,12 +38,39 @@ inline void Logistic(const Shape &input_shape, const float *input_data, const Sh
   auto output_map = MapAsVector(output_data, output_shape);
   output_map.array() = input_map.array().unaryExpr(Eigen::internal::scalar_logistic_op<float>());
 #else
-  // Note, this can be done using TANH: (1/2) + (1/2) * TANH(x/2)
-  const int size = MatchingFlatSize(input_shape, output_shape);
-  for (int i = 0; i < size; i++)
-  {
-    output_data[i] = 1.f / (1.f + std::exp(-input_data[i]));
-  }
+        size_t elements = input_shape.FlatSize(); //input->bytes / sizeof(float);
+        const float* in = input_data; //input->data.f;
+        float* out = output_data; //output->data.f;
+        int nn = elements >> 2;
+        int remain = elements - (nn << 2);
+        float32x4_t _one = vdupq_n_f32(1.f);
+        UNUSED_RELEASE(output_shape);
+        for (; nn>0; nn--)
+        {
+            float32x4_t _p = vld1q_f32(in);
+            _p = vnegq_f32(_p);
+            _p = arm_compute::vexpq_f32(_p);
+            _p = vaddq_f32(_p, _one);
+            float32x4_t _outp = arm_compute::vinvq_f32(_p);
+            vst1q_f32(out, _outp);
+            in += 4;
+            out += 4;
+        }
+        for (; remain>0; remain--)
+        {
+            *out = 1.f / (1.f + std::exp(-*in));
+            in++;
+            out++;
+        }
+
+
+  
+  /* // Note, this can be done using TANH: (1/2) + (1/2) * TANH(x/2) */
+  /* const int size = MatchingFlatSize(input_shape, output_shape); */
+  /* for (int i = 0; i < size; i++) */
+  /* { */
+  /*   output_data[i] = 1.f / (1.f + std::exp(-input_data[i])); */
+  /* } */
 #endif
 }
 
